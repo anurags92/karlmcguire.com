43b441326b611dfe11a6ee90cecc75a159f5e844
Karl McGuire <karl@xba.io>
2019-04-24 22:30:17 -0400

    working on lua parser

--- mock/main.lua
+++ mock/main.lua
@@ -1,6 +1,50 @@
-#!/usr/local/bin/lua -i
+#!/usr/local/bin/lua
 
+local mark = require("markdown")
 local toml = require("toml")
-local temp = toml.parse('head = "Hello, world!"')
 
-print(temp["head"])
+-- trim removes the surrounding whitespace from a string
+function trim(s)
+  return (s:gsub("^%s*(.-)%s*$", "%1"))
+end
+
+-- post loads a post from path and returns a table representation of it, with
+-- the corresponding meta information and parsed markdown as html
+function post(path)
+  local meta = function(text)
+    return toml.parse(trim(text:sub(5, text:find("+++", 4) - 2)))
+  end
+
+  local html = function(text)
+    return trim(mark(text:sub(text:find("+++", 4) + 3)))
+  end
+
+  local file = io.open(path, "r"):read("*a")
+
+  return {meta = meta(file), html = html(file)}
+end
+
+-- list returns a table of all post relative paths
+function list(path)
+  local temp = {}
+
+  for line in io.popen("ls " .. path):lines() do 
+    temp[#temp + 1] = path .. line
+  end
+
+  return temp
+end
+
+-- posts returns a table of all posts
+function posts(dir)
+  local files = list(dir)
+  local temp = {}
+
+  for i, file in pairs(files) do
+    temp[i] = post(file)
+  end
+
+  return temp
+end
+
+print(posts("posts/")[2].meta.head)
---
+++ mock/posts/one.md
@@ -0,0 +1,15 @@
++++
+head = "Another post!"
+desc = "This is the description for this shit."
+date = "2019-04-25"
+tags = ["two", "four"]
++++
+
+Welcome to my blog! This is the first post, hence the "Hello, world!" title, and
+I hope you enjoy your stay here. This is my first post of many.
+
+## h2
+
+* another
+* another one
+* another another one
