9509f4484a63979908c5e93f04304dc2aad696d9
Karl McGuire <karl@xba.io>
2019-04-25 01:32:56 -0400

    organization

--- mock/main.lua
+++ mock/main.lua
@@ -1,124 +1,5 @@
 #!/usr/local/bin/lua
 
-local snip = {}
-
-function snip.page(head, body)
-  return [[
-  <!doctype html>
-  <html lang="en">
-    ]] .. head .. body .. [[
-  </html>
-  ]]
-end
-
-function snip.head(title)
-  return [[
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>]] .. title .. [[</title>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Mono">
-    <link rel="stylesheet" href="/index.css">
-    <script src="/index.js"></script>
-  </head>
-  ]]
-end
-
-function snip.body(header, main)
-  return [[
-  <body>
-    <div class="wrap">
-  ]] .. header .. main .. [[
-      <footer>
-        &copy; 2019 Karl McGuire
-        <span>Powered by <a href="https://lua.org">Lua</a>.</span>
-      </footer>
-    </div>
-  </body>
-  ]]
-end
-
-function snip.header(active)
-  local function curr(path)
-    if active == path then return [[ class="active"]] else return "" end
-  end
-
-  return [[
-  <header>
-    <nav>
-      <div>
-        <a href="/"]] .. curr("index") ..[[>Karl McGuire</a>
-      </div>
-      <div>
-        <a href="/about/"]] .. curr("about") .. [[>About</a>
-        <a href="/contact/"]] .. curr("contact") .. [[>Contact</a>
-      </div>
-    </nav>
-  </header>
-  ]]
-end
-
-function snip.map()
-  local boxes = ""
-
-  for i = 1,7 do
-    boxes = boxes .. [[<div class="map__row">]]
-    for a = 1, 52 do
-      boxes = boxes .. 
-        [[<div id="box__]] .. (i .. "__" .. a) .. [[" class="map__box"></div>]]
-    end
-    boxes = boxes .. [[</div>]]
-  end
-
-  return [[
-  <div class="map">
-  ]] .. boxes .. [[
-  </div>
-  ]]
-end
-
-function snip.posts(data)
-  local function render_href(path)
-    return path:sub(1, path:len()-3) .. "/"
-  end
-
-  local function render_post(data)
-    return [[
-    <li class="post">
-      <div class="post__title">
-        <a href="]] .. render_href(data.path) .. [[">
-          <span>]] .. data.meta.head .. [[</span>
-        </a>
-      </div>
-      <div class="post__date">
-      ]] .. data.meta.date .. [[
-      </div>
-    </li>
-    ]]
-  end
-
-  local posts = ""
-
-  for _, post in pairs(data) do
-    posts = posts .. render_post(post)
-  end
-
-  return [[
-  <ul class="posts">
-  ]] .. posts .. [[
-  </ul>
-  ]]
-end
-
-function snip.list(map, posts)
-  return [[
-  <main>
-  ]] .. map .. posts .. [[
-  </main>
-  ]]
-end
-
 local mark = require("markdown")
 local toml = require("toml")
 
@@ -166,13 +47,105 @@ function get_posts(dir)
   return posts
 end
 
+function gen_page(title, active, main)
+  local function gen_head(title)
+    return [[<head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>]] .. title .. [[</title>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Mono">
+      <link rel="stylesheet" href="/index.css">
+      <script src="/index.js"></script>
+    </head>]]
+  end
 
+  local function gen_body(header, main)
+    return [[<body>
+      <div class="wrap">
+        ]] .. header .. main .. [[
+        <footer>
+          &copy; 2019 Karl McGuire
+          <span>Powered by <a href="https://lua.org">Lua</a>.</span>
+        </footer>
+      </div>
+    </body>]]
+  end
 
-print(
-  snip.page(
-    snip.head("Karl McGuire"),
-    snip.body(
-      snip.header("index"),
-      snip.list(
-        snip.map(),
-        snip.posts(get_posts("posts/"))))))
+  local function gen_header(active)
+    local function curr(path)
+      if active == path then
+        return [[ class="active"]]
+      else
+        return ""
+      end
+    end
+
+    return [[<header>
+      <nav>
+        <div>
+          <a href="/"]] .. curr("index") ..[[>Karl McGuire</a>
+        </div>
+        <div>
+          <a href="/about/"]] .. curr("about") .. [[>About</a>
+          <a href="/contact/"]] .. curr("contact") .. [[>Contact</a>
+        </div>
+      </nav>
+    </header>]]
+  end
+
+  local head   = gen_head(title)
+  local header = gen_header(active)
+  local body   = gen_body(header, main)
+
+  return [[<!doctype html><html lang="en">]] .. head .. body .. [[</html>]]
+end
+
+function gen_list(posts)
+  local function gen_map()
+    local boxes = ""
+
+    for i = 1,7 do
+      boxes = boxes .. [[<div class="map__row">]]
+      for a = 1, 52 do
+        boxes = boxes ..
+          [[<div id="box__]] .. (i .. "__" .. a) .. 
+          [[" class="map__box"></div>]]
+      end
+      boxes = boxes .. [[</div>]]
+    end
+
+    return [[
+    <div class="map">
+    ]] .. boxes .. [[
+    </div>
+    ]]
+  end
+
+  local function gen_post(data)
+    local function gen_href(path)
+      return path:sub(1, path:len()-3) .. "/"
+    end
+
+    return [[<li class="post">
+      <div class="post__title">
+        <a href="]] .. gen_href(data.path) .. [[">
+          <span>]] .. data.meta.head .. [[</span>
+        </a>
+      </div>
+      <div class="post__date">
+      ]] .. data.meta.date .. [[
+      </div>
+    </li>]]
+  end
+
+  local list = ""
+
+  for _, post in pairs(posts) do
+    list = list .. gen_post(post)
+  end
+
+  return gen_map() .. [[<ul class="posts">]] .. list .. [[</ul>]]
+end
+
+print(gen_page("Karl McGuire", "index", gen_list(get_posts("posts/"))))
